9fb5ce956301c3815d50c1f1bcca3a31cd6e81ae
jrs8205 <jarsi@jarsi.org>
2026-07-12 14:58:28 +0300

    Prepare the v0.6.0 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "org.jarsi.betascout"
         minSdk = 26
         targetSdk = 36
-        versionCode = 10
-        versionName = "0.5.1"
+        versionCode = 11
+        versionName = "0.6.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
